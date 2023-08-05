575dcfa620da60ae42e83ac852fd0ffb07faa926
Preisler <preisler.andras@gmail.com>
2023-08-05 12:01:53 +0200

    main

---
+++ src/components/containers/mainContainer/MainContainer.js
@@ -0,0 +1,14 @@
+import './MainContainer.css'
+
+const MainContainer = () => {
+    return (
+        <div className="main-cont">
+            <div className="main-cont-effect">
+
+            </div>
+        </div>
+    );
+}
+
+export default MainContainer;
+
---
+++ src/components/containers/taskContainer/TaskContainer.js
@@ -0,0 +1,7 @@
+const taskContainer = () => {
+    return (
+        <div>
+            <Task />
+        </div>
+    );
+}
