58249087cf4b69a302f926e4054f7ab1b9d4ef4d
Preisler <preisler.andras@gmail.com>
2023-09-02 09:49:39 +0200

    main

--- src/Pages/dailyPointPage.js
+++ src/Pages/dailyPointPage.js
@@ -39,7 +39,8 @@ const DailyPointPage = () => {
     return (
         <div className="main-cont">
             <Navbar day={day} setDay={setDay} all_days={all_days} view={view} setView={setView} all_views={all_views} data={data} setData={setData} all_data={all_data} />
-            <Body url={url} />
+            <Body ur
+                l={url} />
             <Footer />
         </div>
     );
